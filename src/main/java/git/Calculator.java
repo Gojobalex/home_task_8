bc233e317df5686a056347fbf917224eb644a880
Gojobalex <64366950+Gojobalex@users.noreply.github.com>
2021-07-07 20:26:51 +0300

    comit

--- src/main/java/git/Calculator.java
+++ src/main/java/git/Calculator.java
@@ -2,6 +2,8 @@ package git;
 
 public class Calculator {
     public void start() {
-
+        System.out.println("1");
+        System.out.println("12");
+        System.out.println("123");
     }
 }
